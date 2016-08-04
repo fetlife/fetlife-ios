4d04b8e9f4715011b80c763a4ab23cbccf1866ec
Jeff Trespalacios <jeff.trespalacios@gmail.com>
2016-08-04 07:47:56 -0700

    User can use 1Password to fill in their credentials

--- FetLife/ViewControllers/LoginViewController.swift
+++ FetLife/ViewControllers/LoginViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import p2_OAuth2
+import OnePasswordExtension
 
 class LoginViewController: UIViewController {
     
@@ -15,6 +16,8 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var devilHeartImage: UIImageView!
     @IBOutlet weak var loginButton: UIButton!
+    private weak var loginWebViewController:  UIViewController?
+    private weak var loginWebView: UIWebView?
     
     // MARK: - View lifecycle
     
@@ -26,15 +29,59 @@ class LoginViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
+    override func presentViewController(viewControllerToPresent: UIViewController, animated flag: Bool, completion: (() -> Void)?) {
+        
+        defer {
+            super.presentViewController(viewControllerToPresent, animated: flag, completion: completion)
+        }
+        
+        guard OnePasswordExtension.sharedExtension().isAppExtensionAvailable() else { return }
+        guard let presentedNav = viewControllerToPresent as? UINavigationController,
+            topViewController = presentedNav.topViewController,
+            webview = presentedNav.topViewController?.view.subviews.first as? UIWebView else {
+                return
+        }
+        
+        loginWebViewController = topViewController
+        loginWebView = webview
+        
+        let navBar = presentedNav.navigationBar
+        let button = UIButton()
+        let image = UIImage(named: "onepassword")?.imageWithRenderingMode(.AlwaysTemplate)
+        button.setImage(image, forState: .Normal)
+        button.tintColor = UIColor.brickColor()
+        button.addTarget(self, action: #selector(LoginViewController.onePasswordTriggered), forControlEvents: .TouchUpInside)
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        navBar.addSubview(button)
+        button.centerYAnchor.constraintEqualToAnchor(navBar.centerYAnchor).active = true
+        button.leftAnchor.constraintEqualToAnchor(navBar.leftAnchor, constant: 10).active = true
+    }
+    
     // MARK: - Actions
     
     @IBAction func login(sender: UIButton) {
         sender.setTitle("Authorizing...", forState: .Normal)
         
         API.authorizeInContext(self,
-            onAuthorize: { parameters in self.didAuthorizeWith(parameters) },
-            onFailure: { error in self.didCancelOrFail(error) }
+                               onAuthorize: { parameters in self.didAuthorizeWith(parameters) },
+                               onFailure: { error in self.didCancelOrFail(error) }
         )
+    }
+    
+    func onePasswordTriggered(sender: UIButton) {
+        guard let loginWebView = loginWebView, loginWebViewController = loginWebViewController else {
+            return
+        }
+        
+        OnePasswordExtension.sharedExtension().fillItemIntoWebView(loginWebView,
+                                                                   forViewController: loginWebViewController,
+                                                                   sender: sender,
+                                                                   showOnlyLogins: false) { (success, error) in
+                                                                    if !success {
+                                                                        print("Failed to fill into webview: \(error)")
+                                                                    }
+        }
     }
     
     func didAuthorizeWith(parameters: OAuth2JSON) {
