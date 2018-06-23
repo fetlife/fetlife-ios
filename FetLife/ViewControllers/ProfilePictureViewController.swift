1ae52dd9bc562035c395bccff063615bc554d234
Matt <whitehat007@users.noreply.github.com>
2018-06-23 02:56:46 -0700

    Remove unused variable

--- FetLife/ViewControllers/ProfilePictureViewController.swift
+++ FetLife/ViewControllers/ProfilePictureViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 class ProfilePictureViewController: UIViewController, UIScrollViewDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var navBar: UINavigationBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
